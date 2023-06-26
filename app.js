5ed1852ca0ad9bab6d70fbafa758110bd25fb5b8
James <>
2023-06-25 23:32:09 -0500

    completed basic project requirements

--- app.js
+++ app.js
@@ -1,7 +1,87 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// const morgan = require('morgan');
+// app.use(morgan('dev'));
+
+const volleyball = require('volleyball');
+app.use(volleyball)
+
+app.use(express.static('public'))
+
+const postBank = require('./postBank')
+
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+  </html>`;
+
+  res.status(200).send(html);
+});
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id
+  const post = postBank.find(id);
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header> 
+        <div class='news-item'>
+          <p>
+          
+            ${post.title}
+            
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>  
+    </div>
+  </body>
+  </html>`;
+  if (!post.id) {
+    throw new Error('not found')
+  } else {
+  res.send(html) }
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Post not found!')
+})
 
 const PORT = 1337;
 
